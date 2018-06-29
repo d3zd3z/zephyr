f6b895e15908630dfc986ed1ddcf6a87274deef6
David Brown <david.brown@linaro.org>
2018-06-29 11:35:25 -0600

    Hack: Add clock stuff to hello world

--- samples/hello_world/src/main.c
+++ samples/hello_world/src/main.c
@@ -6,8 +6,12 @@
 
 #include <zephyr.h>
 #include <misc/printk.h>
+#include <time.h>
 
 void main(void)
 {
 	printk("Hello World! %s\n", CONFIG_ARCH);
+
+	time_t now = time(NULL);
+	printk("Time gotten is %ld\n", now);
 }
