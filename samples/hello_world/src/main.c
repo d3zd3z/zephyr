3b7dc0a5f3bcb595d297ab6ebe3250c923067c3b
David Brown <david.brown@linaro.org>
2020-04-21 10:18:07 -0600

    Add a hacky flash open test

--- samples/hello_world/src/main.c
+++ samples/hello_world/src/main.c
@@ -6,6 +6,7 @@
 
 #include <zephyr.h>
 #include <sys/printk.h>
+#include <storage/flash_map.h>
 
 void showit(uintptr_t offset) {
 	uint32_t *addr = (uint32_t *)offset;
@@ -14,17 +15,33 @@ void showit(uintptr_t offset) {
 	printk("    Got: 0x%08x\n", value);
 }
 
+#if 0
+void flashy(void) {
+	int ret;
+	const struct flash_area *fa;
+
+	ret = flash_area_open(SOC_FLASH_0_ID, &fa);
+	printk("flash_area_open: %d\n", ret);
+	if (ret != 0) {
+		return;
+	}
+}
+#endif
+
 void main(void)
 {
 	printk("Hello World! %s\n", CONFIG_BOARD);
 	printk("MPU: 0x%08x\n", *((uint32_t *) 0xe000ed94));
 
+	// flashy();
+#if 0
 	/* Try disabling the MPU */
 	// *((uint32_t *) 0xe000ed94) = 0;
 	printk("MPU: 0x%08x\n", *((uint32_t *) 0xe000ed94));
 	for (uintptr_t offset = 4; offset < 0x80000; offset *= 2) {
 		showit(offset);
 	}
+#endif
 #if 0
 	showit(0);
 	showit(0x0fff8);
